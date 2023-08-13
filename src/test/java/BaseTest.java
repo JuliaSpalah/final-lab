9ce636962d06cd3a57df77f710d0c320dfc1bb39
Yulia <julia.pikina@gmail.com>
2023-08-13 21:24:41 +0300

    Screenshots with Allure unsuccessful

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,19 @@
-import org.apache.log4j.PropertyConfigurator;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.prestashop.BasePage;
 import org.prestashop.BrowserFactory;
 import org.prestashop.BrowserTypes;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
+import java.util.Arrays;
 
 public class BaseTest {
     @BeforeMethod
@@ -20,11 +26,15 @@ public class BaseTest {
         driver.get("https://demo.prestashop.com/");
         driver.manage().window().setSize(new Dimension(width, height));
         BasePage.setDriver(driver);
-        PropertyConfigurator.configure("test/java/log4j.properties");
     }
 
     @AfterMethod
-    public void quite() {
+    public synchronized void quite(ITestResult testResult) throws IOException {
+        if (testResult.getStatus() == ITestResult.FAILURE) {
+            File scrFile = ((TakesScreenshot) BasePage.getDriver()).getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(scrFile, new File("target/errorScreenshots/" + testResult.getName() + "-"
+                    + Arrays.toString(testResult.getParameters()) + ".jpg"));
+        }
         BasePage.getDriver().quit();
     }
 }
--- src/test/java/TestAddingToCart.java
+++ src/test/java/TestAddingToCart.java
@@ -3,7 +3,8 @@ import org.prestashop.MainPage;
 import org.prestashop.SearchResultPage;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class TestAddingToCart extends BaseTest {
 
